63b8ac14f07d3916c3d57a0a8c5ff604214ec6bd
Archer <archerevil2005@gmail.com>
2022-05-06 21:37:50 +0000

    FInal Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ImageToAsciiArt from 'image-to-ascii-art'
+import { useState } from 'react';
+import axios from 'axios';
+import parse from'html-react-parser';
 
-function App() {
+const App:React.FC = () => {
+
+  const [Url, setUrl] = useState('');
+  const [Image, setImage] = useState('');
+  const [bgColor, setbgColor] = useState('#FF0000');
+  const convert = () => {
+    console.log(bgColor)
+
+    // const link = 'https://process.filestackapi.com/AIMhUFS9mRe2A7DJy0NIkz/ascii=background:'+ bgColor +'/' + Url
+    const link = 'https://process.filestackapi.com/AIMhUFS9mRe2A7DJy0NIkz/ascii=colored:true,background:'+ bgColor.toString().substring(1) +'/' + Url
+    console.log(link)
+    axios.get(link).then((response: any) => {
+      setImage(response.data)
+    })
+    
+
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className={'main'}>
+      <p>Image to ASCII App</p>
+      <h2>URL:</h2>
+      <input type={'text'} onChange={e => {setUrl(e.target.value)}} id="myFile" name="filename"
+      />
+      <div>
+      <h2>Background Color:</h2>
+      <input type={'color'} onChange={e => {setbgColor(e.target.value)}}/>
+      </div>
+      <button onClick={() => convert()} className="btn"> Convert
+</button>
+      <h2>Your ASCII Text:</h2>
+      {parse(Image)}
+    </main>
   );
 }
 
